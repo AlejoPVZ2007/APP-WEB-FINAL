69b2cc522fb5c8996c36850e4a3508f41a497c55
AlejoPVZ2007 <amatamoro@isise.edu.pe>
2026-03-23 10:41:14 -0500

    Confianza

--- src/app/paginas/emociones/confianza/confianza.ts
+++ src/app/paginas/emociones/confianza/confianza.ts
@@ -1,9 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';   // 👈 para *ngFor
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-confianza',
-  imports: [],
+  standalone: true,  // 👈 IMPORTANTE
+  imports: [CommonModule, FormsModule], // 👈 AQUÍ VA LA SOLUCIÓN
   templateUrl: './confianza.html',
-  styleUrl: './confianza.css',
+  styleUrls: ['./confianza.css']
 })
-export class Confianza {}
+export class Confianza implements OnInit {
+
+  mensaje: string = '';
+
+  comentarios = [
+    {
+      usuario: 'Usuario123',
+      texto: 'Hoy me sentí más seguro de mí mismo 🌟',
+      tiempo: 'Hace 2 minutos'
+    },
+    {
+      usuario: 'Ana',
+      texto: 'Me sentí apoyada por mi familia 🌿',
+      tiempo: 'Hace 10 minutos'
+    }
+  ];
+
+  ngOnInit(): void {
+    this.iniciarLluvia();
+  }
+
+  publicar() {
+    if (this.mensaje.trim() !== '') {
+      this.comentarios.unshift({
+        usuario: 'Tú',
+        texto: this.mensaje,
+        tiempo: 'Ahora'
+      });
+      this.mensaje = '';
+    }
+  }
+
+  iniciarLluvia() {
+    console.log("Animación de lluvia iniciada");
+  }
+}
